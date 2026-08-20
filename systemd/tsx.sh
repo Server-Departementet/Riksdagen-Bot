61546c93a62da4765e8106d95323d4f09a4ba0c4
VenaStrom <vena.strom+github@pm.me>
2026-08-20 16:24:17 +0200

    invoke tsx directly in cron (yarn leaks tmpfs temp dirs)

--- systemd/tsx.sh
+++ systemd/tsx.sh
@@ -7,5 +7,7 @@ export NVM_DIR="$HOME/.nvm"
 # Change to project directory
 cd "$HOME/Riksdagen-Bot" || exit 1
 
-# Run the script passed as argument
-exec yarn tsx "$@"
+# Run the script passed as argument. tsx is invoked directly instead of via
+# `yarn tsx`: yarn leaks xfs-* temp dirs into /tmp (a tmpfs) on every run,
+# which slowly eats the container's RAM.
+exec ./node_modules/.bin/tsx "$@"
